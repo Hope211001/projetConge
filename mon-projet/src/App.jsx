8df974741bd8ab2d82fd30d5cf6bedfbaf27003e
Hope211001 <fanantenanarafaniritsoa@gmail.com>
2025-02-11 13:00:24 +0300

    io

--- mon-projet/src/App.jsx
+++ mon-projet/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MyCars from './components/MyCars'
 
-import AppRoute from "./route/AppRoute"
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <AppRoute/>
-    </div>
+    <MyCars/>
   )
 }
 export default App
--- mon-projet/src/components/Car.jsx
+++ mon-projet/src/components/Car.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-
+import "../assets/css/car.css"
 function Car({ couleur, marque, plaque, image }) {
     return (
-        <div className="flex flex-wrap justify-center gap-4">
-            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-                <div className="bg-white shadow-lg rounded-2xl overflow-hidden">
-                    <img
-                        src={image}
-                        className="w-full h-48 object-cover"
-                        alt={`Image of the car ${marque}`}
-                    />
-                    <div className="p-4">
-                        <h5 className="text-lg font-bold text-gray-800">Marque: {marque}</h5>
-                        <p className="text-gray-600">Couleur: {couleur}</p>
-                        <p className="text-gray-600">Plaque: {plaque}</p>
-                    </div>
+        <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+                <img src={image} className="card-img-top imageCar" alt={`Image de la voiture ${marque}`} />
+                <div className="card-body">
+                    <h5 className="card-title">Marque: {marque}</h5>
+                    <p className="card-text">Couleur: {couleur}</p>
+                    <p className="card-text">Plaque: {plaque}</p>
                 </div>
             </div>
         </div>
-    );
+    )
 }
-
-export default Car;
+export default Car
